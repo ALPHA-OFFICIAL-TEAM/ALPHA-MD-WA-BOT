f75b4639e08eeafc5d721b784146d541f2c83146
kddrfake <99086051+kddrfake@users.noreply.github.com>
2022-06-21 11:44:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,18 +12,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
-global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
-global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
-global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
+global.owner = ['94703199484']
+global.premium = ['94703199484']
+global.ownernomer = '94703199484'
+global.ownername = 'Dineth Randinu'
+global.botname = '🎭KDDR-fake🎭 v1.0.0'
+global.footer = ' 🎭KDDR-fake🎭'
+global.ig = 'https://github.com/kddrfake'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/ALPHA-OFFICIAL-TEAM'
-global.myweb = 'https://www.youtube.com/c/AchiBrolk'
-global.packname = 'ALPHA WA BOT MD'
-global.author = 'D.U.A.N.FERNANDO '
+global.sc = 'https://github.com/kddrfake'
+global.myweb = 'https://www.youtube.com/c/⚡KDDR FAKE⚡'
+global.packname = 'KDDR WA BOT MD'
+global.author = 'K.D.D.RANDINU '
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
